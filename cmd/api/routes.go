1de89b441449a5dc1c7508c8c936faff19df6214
chlovec <alfred.okoronkwo@gmail.com>
2025-07-21 09:13:36 -0400

    rate limit

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -20,5 +20,5 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.patchMovieHandler)
 
-	return app.recoverPanic(router)
+	return app.recoverPanic(app.rateLimit(router))
 }
